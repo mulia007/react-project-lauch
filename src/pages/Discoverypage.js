1521852dc877fc4d90147f9af19861a0b87f50f8
mulia007 <116814230+mulia007@users.noreply.github.com>
2023-01-02 01:54:46 +0700

    Update Discoverypage.js

--- src/pages/Discoverypage.js
+++ src/pages/Discoverypage.js
@@ -5,7 +5,7 @@ const Discoverypage = () => {
   return (
     <div>
       <Navbar />
-      <h1>this is Discovery oke oke </h1>
+      <h1>this is Discovery</h1>
       <h2>ya</h2>
     </div>
   );
